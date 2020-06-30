92180a429c16a5c09eb84282508debe4fad96f8c
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-29 20:26:10 -0600

    Create template skeleton

---
+++ src/components/Todos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div v-for='todo in todos' v-bind:key='todo.id'>
+      <p>{{ todo.title }}</p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'Todos',
+    props: ['todos']
+  }
+</script>
+
+<style scoped>
+
+</style>
